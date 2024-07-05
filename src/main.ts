7037d4eb817e5ccbc63e8fd28f99c4876985ff6e
xiaobo <zhaoboqi77@163.com>
2024-07-05 21:25:54 +0800

    新增主页，以及配置重定向

--- src/main.ts
+++ src/main.ts
@@ -8,10 +8,12 @@ import router from './router'
 import '@arco-design/web-vue/dist/arco.css'
 import '@/assets/styles/main.css'
 
+const pinia = createPinia()
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(ArcoVue)
+app.use(pinia)
 
 app.mount('#app')
